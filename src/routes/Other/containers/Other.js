9d5ad4463cc3d7a87c1f982bbf726c693d3d4ca0
John Iaconis <john.iaconis6sense.com@ip-192-168-1-8.ec2.internal>
2021-03-31 10:51:07 -0700

    add interview data

--- src/routes/Other/containers/Other.js
+++ src/routes/Other/containers/Other.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import episodes from './episodes.json';
 
 export function Other() {
+  console.log('episodes', episodes);
   return (
     <div>
       Other Page :D
